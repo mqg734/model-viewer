6cce03e2c6c7319fcff0e2134bdbd73052b5c680
chrismgeorge <chrismgeorge@users.noreply.github.com>
2020-10-01 18:40:21 +0000

    deploy: f6570413d481de4344db81d37983cebff9cd5bbc

---
+++ lib/docs-and-examples/create-html.d.ts
@@ -0,0 +1,1 @@
+export declare function convertJSONToHTML(json: any[], docsOrExamples: 'docs' | 'examples'): void;
---
+++ lib/docs-and-examples/utils.d.ts
@@ -0,0 +1,3 @@
+export declare function switchPages(oldType: string, newType: string): void;
+export declare function loadExamples(): Promise<void>;
+export declare function init(docsOrExamples: 'docs' | 'examples'): void;
